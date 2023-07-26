ef1a60e2d42ca6fd8c1076f88733070a0a09fc5c
wasa wasab <treenoise09@gmail.com>
2023-07-26 20:52:18 +0700

    change:Toast color more red and more green,Modal change to spinner no background

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -3,23 +3,23 @@ import React from 'react';
 export const Toast = ({ messages }) => {
   const getToastClasses = (isError) => {
     return isError
-      ? 'text-red-800 border-red-300 bg-red-50 dark:text-red-400 dark:bg-gray-800 dark:border-red-800'
-      : 'text-green-800 border-green-300 bg-green-50 dark:text-green-400 dark:bg-gray-800 dark:border-green-800';
+      ? 'text-red-8000 border-red-3000 bg-red-500 dark:text-red-400 dark:bg-gray-800 dark:border-red-800'
+      : 'text-green-8000 border-green-3000 bg-green-500 dark:text-green-400 dark:bg-gray-800 dark:border-green-800';
   };
 
   const getButtonClasses = (isError) => {
     return isError
-      ? 'bg-red-50 text-red-500 focus:ring-red-400 hover:bg-red-200 dark:text-red-400 dark:bg-gray-800 dark:hover:bg-gray-700'
-      : 'bg-green-50 text-green-500 focus:ring-green-400 hover:bg-green-200 dark:text-green-400 dark:bg-gray-800 dark:hover:bg-gray-700';
+      ? 'bg-red-5000 text-red-5000 focus:ring-red-400 hover:bg-red-400 dark:text-red-400 dark:bg-gray-800 dark:hover:bg-gray-700'
+      : 'bg-green-5000 text-green-5000 focus:ring-green-400 hover:bg-green-200 dark:text-green-400 dark:bg-gray-800 dark:hover:bg-gray-700';
   };
 
   return messages.map((msg, index) => (
     <div 
       id={`alert-${index}`} 
-      className={`flex items-center p-4 mb-4 border-t-4 ${getToastClasses(msg.isError)}`} 
+      className={`flex items-center p-4 mb-4 ${getToastClasses(msg.isError)}`} 
       role="alert"
     >
-      <div className="ml-3 text-sm font-medium">{msg.message}</div>
+      <div className="ml-3 text-ml font-medium">{msg.message}</div>
       <button 
         type="button" 
         className={`ml-auto -mx-1.5 -my-1.5 rounded-lg p-1.5 inline-flex items-center justify-center h-8 w-8 ${getButtonClasses(msg.isError)}`}
